fc709b969356652abf27d6a83768f64764721749
DaejonFerrao <daejonferrao@gmail.com>
2024-08-14 22:57:12 +0100

    started mobile development

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -17,13 +17,17 @@ const About = () => {
             high-quality fruits and
             vegetables straight from the farm to your table
         </p>
+        <div className="mobile-title">Natural Fresh and Local</div>
         <div className="property">
             <img src={tick} alt="tick" className="tick" />
             <p className="tick-description">Only the finest produce, handpicked for your table</p>
+            <p2 className="mobile-description">Quality</p2>
             <img src={bucket} alt="tick" className="tick" />
             <p className="tick-description">Fresh fruits and veggies, packed with flavour</p>
+            <p2 className="mobile-description">Fruit & Veg</p2>
             <img src={boxes} alt="tick" className="tick" />
             <p className="tick-description">Top-grade produce, reliably delivered</p>
+            <p2 className="mobile-description">Delivery</p2>
         </div>
         <div className="button-container">
             <button className="about-button">View Our Produce</button>
